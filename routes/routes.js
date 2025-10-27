d9b763fd352d00118753e8d4b7352749d9ec423d
Matheus.R.Brandao <153210805+MatBrand10@users.noreply.github.com>
2025-10-27 19:38:39 -0300

    Delete routes directory

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require('../controllers/homeController');
-const sobreController = require('../controllers/sobreController');
-
-router.get('/', homeController.getHome);
-router.get('/sobre', sobreController.getSobre);
-
-module.exports = router;
